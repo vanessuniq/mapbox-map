b2aa3785537869734fc66cefb5f69ded53a45fd3
vanessuniq <vanessuniq@gmail.com>
2020-12-18 14:55:48 -0500

    update coordinate

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const initMap = () => {
     const map = new mapboxgl.Map({
         container: "map",
         style: "mapbox://styles/mapbox/dark-v10",
-        center: [4.900293, 52.379174],
-        zoom: 12.5,
+        center: [-77.036698, 38.897102],
+        zoom: 7,
     });
 
     const popup = new mapboxgl.Popup().setHTML(
-        `<h2>Amsterdam central</h2><p>Station</p>`
+        `<h2>Washington, DC</h2><p>District Capital</p>`
     );
 
     const marker = new mapboxgl.Marker()
-        .setLngLat([4.900293, 52.379174])
+        .setLngLat([-77.036698, 38.897102])
         .setPopup(popup)
         .addTo(map);
 };
